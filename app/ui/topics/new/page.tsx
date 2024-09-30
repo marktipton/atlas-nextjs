7fb7799525ff9c315245b6d52bdcbada8c291c7b
marktipton <marktipton95@gmail.com>
2024-09-30 14:07:09 -0500

    added a server action to create a topic page

--- app/ui/topics/new/page.tsx
+++ app/ui/topics/new/page.tsx
@@ -1,6 +1,7 @@
 import CreateTopicForm from "@/components/CreateTopicForm";
+export default async function Page() {
+  await new Promise((r) => setTimeout(r, 3000));
 
-export default function Page() {
   return (
     <div>
       <h1 className={`mb-4 text-xl md:text-2xl`}>New Topic</h1>
