ab71e104f5ff5de1b00292ac9f4b2e7869a9165a
marktipton <marktipton95@gmail.com>
2024-09-30 13:46:20 -0500

    added questions from database to topic pages

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { AskQuestion } from "@/components/AskQuestion";
+import { Question } from "@/components/Question";
+import { fetchQuestions, fetchTopic } from "@/lib/data";
+import { HashtagIcon } from "@heroicons/react/24/outline";
 
-async function page({params}: { params: { id: string }}) {
+export default async function Page({ params }: { params: { id: string } }) {
+  const topic = await fetchTopic(params.id);
+  const questions = await fetchQuestions(params.id);
 
-  await new Promise((r) => setTimeout(r, 3000));
+  if (!topic) {
+    return <div>Topic not found</div>;
+  }
+
   return (
-    <div>Topics Page: {params.id}</div>
-  )
+    <div>
+      <h1 className="text-3xl font-black flex items-center">
+        <HashtagIcon className="h-6 w-6 mr-2" /> {topic.title}
+      </h1>
+      <AskQuestion topic={topic.id} />
+      {questions.map((question) => (
+        <Question
+          key={question.id}
+          id={question.id}
+          text={question.title}
+          votes={question.votes}
+        />
+      ))}
+    </div>
+  );
 }
-
-export default page
