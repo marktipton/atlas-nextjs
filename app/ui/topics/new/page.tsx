5911d58865ddfc1ab55e951022c982ef2a024d3e
marktipton <marktipton95@gmail.com>
2024-09-30 14:00:22 -0500

    added form to new topic page

--- app/ui/topics/new/page.tsx
+++ app/ui/topics/new/page.tsx
@@ -1,5 +1,10 @@
-export default async function Page() {
-  await new Promise((r) => setTimeout(r, 3000));
+import CreateTopicForm from "@/components/CreateTopicForm";
 
-  return (<div>New Topics Page</div>);
+export default function Page() {
+  return (
+    <div>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>New Topic</h1>
+      <CreateTopicForm />
+    </div>
+  );
 }
