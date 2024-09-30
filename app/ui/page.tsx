f778a60e4045eff81ca39252aae818112feacbb8
marktipton <marktipton95@gmail.com>
2024-09-30 13:42:24 -0500

    added database topics to homepage

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -1,5 +1,14 @@
-export default async function Page() {
-  await new Promise((r) => setTimeout(r, 3000));
+import { Topic } from "@/components/Topic";
+import { fetchTopics } from "@/lib/data";
 
-  return (<div>Topics Page</div>);
+export default async function Page() {
+  const topics = await fetchTopics();
+  return (
+    <main className="flex flex-col items-stretch justify-stretch">
+      <h1 className="mb-4 text-xl md:text-2xl">Topics</h1>
+      {topics.map((topic) => (
+        <Topic key={topic.id} id={topic.id} text={topic.title} />
+      ))}
+    </main>
+  );
 }
