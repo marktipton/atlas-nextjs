b41ac996f5b91489aec506d4e6ae4c5ce36b9ef3
marktipton <marktipton95@gmail.com>
2024-09-30 14:40:54 -0500

    added authjs api routes

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ export { auth as middleware } from "@/auth";
 
 export const config = {
   matcher: [
+    // allows home page and about page to be accessed by non-users
     "/((?!api|_next/static|_next/image|favicon.ico|logo.png|about|$).*)",
   ],
 };
